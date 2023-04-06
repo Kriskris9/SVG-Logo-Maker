5ca3278c330d0d77c9d0fdecd35ae9718ce4d291
Kristina Ahn <kristinaahn@Kristinas-iMac.local>
2023-04-06 00:56:13 -0700

    added readme skeleton

--- shapes.js
+++ shapes.js
@@ -1,65 +1,65 @@
 class SVG {
 
   constructor() {
-    this.fillcolor = "";
     this.shape = "";
     this.text = "";
-    this.textColor = "";
-  }
-  setColor(color) {
-    this.fillColor = color;
-  }
+  }  
+
   setText(message, color) {
-    this.textColor = textColor;
     if (message.length > 3) {
       throw new Error("Text must not exceed 3 characters.");}
-    this.text = `<text x="150" y="125" font-size="60" text-anchor="middle" fill="${this.textColor}"> ${this.text} </text>`;
-    
-    
-  }
-  setShape(shape) {
-    this.shape = shape; 
-  }
-  render() { 
-    return `<svg version="1.1"
-        width="300" height="200"
-        xmlns="http://www.w3.org/2000/svg"> ${this.shape}${this.text} </svg>`;
-  }
-}
-
-
-
-class Circle extends SVG {
-  constructor() {
-    
-  }
-
-  getShape() {
-    return `<${this.shape} cx="150" cy="100" r="80" fill="${this.fillColor}" />`;
-  }
-
-  getShapeClass() {
-    // return 'Circle'
-  }
-}
-
-class Triangle extends SVG {
-  constructor() {}
-    render() {
-      return `<polygon points="150, 18 244, 182 56, 182" fill="${this.fillColor}" />`;
-    }
-  }
-
-class Square extends SVG {
-  constructor() {
+    this.text = `<text x="150" y="125" font-size="60" text-anchor="middle" fill="${color}">${message}</text>`;
   
   }
+  setShape(shape) {
+    this.shape = shape.render();
+  }
+  render() { 
+    return `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">${this.shape}${this.text}</svg>`;
+  }
+}
+
+
+
+class Circle {
+  constructor() {
+    this.color = null;
+  }
+setColor(color) {
+    this.color = color;
+  }
+
+  render() { 
+   return `<circle cx="150" cy="100" r="80" fill="${this.color}" />`;
+  }
+  
+}
+
+class Triangle {
+  constructor() {
+    this.color = "";
+  }
+  setColor(color) {
+    this.color = color;
+  }
     render() {
-      return `<rect x="90" y="40" width="120" height="120" fill="${this.fillColor}" />`;
+      return `<polygon points="150, 18 244, 182 56, 182" fill="${this.color}" />`;
+    }
+  }
+
+class Square {
+  constructor() {
+    this.color = ""
+  }
+  setColor(color) {
+    this.color = color;
+  }
+    render() {
+      return `<rect x="90" y="40" width="120" height="120" fill="${this.color}" />`;
     }
   }
 
 
-module.exports = { SVG, Circle, Triangle, Square }
+module.exports = {SVG,Circle, Triangle, Square }
 
 
